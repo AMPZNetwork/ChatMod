8af068a1240841ffb7a22f0e94d9ef91ee3d1999
burdoto <kaleidox@comroid.org>
2024-09-08 11:19:18 +0200

    improve text component serializer

--- src/api/main/java/com/ampznetwork/chatmod/api/model/ChatMessage.java
+++ src/api/main/java/com/ampznetwork/chatmod/api/model/ChatMessage.java
@@ -1,7 +1,10 @@
 package com.ampznetwork.chatmod.api.model;
 
+import com.ampznetwork.chatmod.api.util.TextComponentDeserializer;
+import com.ampznetwork.chatmod.api.util.TextComponentSerializer;
 import com.ampznetwork.libmod.api.entity.Player;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Value;
 import net.kyori.adventure.text.TextComponent;
 
@@ -10,5 +13,5 @@ public class ChatMessage {
     Player sender;
     String inputString;
     String        plaintext;
-    @JsonDeserialize() TextComponent text;
+    @JsonSerialize(using = TextComponentSerializer.class) @JsonDeserialize(using = TextComponentDeserializer.class) TextComponent text;
 }
---
+++ src/api/test/java/com/ampznetwork/chatmod/test/util/TextComponentSerializationTest.java
@@ -0,0 +1,38 @@
+package com.ampznetwork.chatmod.test.util;
+
+import com.ampznetwork.chatmod.api.model.ChatMessage;
+import com.ampznetwork.chatmod.api.model.ChatMessagePacket;
+import com.ampznetwork.libmod.api.entity.Player;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public class TextComponentSerializationTest {
+    @Test
+    void testSerialize() throws JsonProcessingException {
+        var text = Component.text("arschlöoch", NamedTextColor.RED, TextDecoration.UNDERLINED)
+                .hoverEvent(HoverEvent.showText(Component.text("greeting")))
+                .clickEvent(ClickEvent.runCommand("help"));
+        var msg = new ChatMessage(
+                Player.builder().name("Kaleidox").id(UUID.randomUUID()).build(),
+                "arschlöoch",
+                "arschlöoch",
+                text);
+        var pkt = new ChatMessagePacket(
+                "MC",
+                "global",
+                msg);
+        var mapper = new ObjectMapper();
+        var json   = mapper.writeValueAsString(pkt);
+        System.out.println("json = " + json);
+        var parse = mapper.readValue(json, ChatMessagePacket.class);
+        System.out.println("parse = " + parse);
+    }
+}
